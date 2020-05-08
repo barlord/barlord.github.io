154df2ddae727f6fe2c6de87b5d39c15ab3bfef5
barlord <64486378+barlord@users.noreply.github.com>
2020-05-08 15:39:42 +0200

    todo

---
+++ todo/script.js
@@ -0,0 +1,34 @@
+console.log('R+L by barlord')
+let ticknumber=0
+
+$('#addtodo').click(() =>{
+    let todo= document.getElementById("todo").value
+    console.log("he?")
+    $('ul').append(
+        `<li >${todo}  
+        <img src="/img/pipa.svg" alt="tick" class='done' >
+        
+        <img src="/img/iconfinder_25_2135797.svg" alt="tick" class='delete' class="alap" >
+       
+        `)
+    
+        $('#todo').val('')
+   
+  
+        
+      
+
+})
+
+$('.todos-container').on('click', '.done', (event) => {
+    $(event.target).parent().toggleClass('completed')
+    console.log("jó?")
+    
+  });
+
+  $('.todos-container').on('click', '.delete', (event) => {
+    $(event.target).parent().toggleClass('deleted')
+    console.log("nem")
+    
+  });
+  
